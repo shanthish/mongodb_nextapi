a2fe21d4e5504238b132ed79864fda881c36b6eb
shanthish23 <shanthish.sr2023cse@sece.ac.in>
2025-07-17 15:47:14 +0530

    fourth commit

--- app/api/product/[id]/route.js
+++ app/api/product/[id]/route.js
@@ -1,11 +1,11 @@
 // import { products } from "@/lib/products";
 import { connectDB } from "@/lib/db";
-import { productModel } from "@/model/productModel";
+import { ProductModel } from "@/model/productmodel";
 
 export let GET = async(_,{params}) =>{
     try{
         await connectDB();
-        const product = await productModel.findById(params.id);
+        const product = await ProductModel.findById(params.id);
         if(!product)
             return Response.json({status:"Wrong request",message:"Please check your id"},{status:400});
         return Response.json({
@@ -29,7 +29,7 @@ export let PATCH = async(req,{params}) => {
     try{
         await connectDB();
         const data = await req.json();
-        let product=await productModel.findByIdAndUpdate(params.id,data);
+        let product=await ProductModel.findByIdAndUpdate(params.id,data);
         if(!product)
             return Response.json({status:"Wrong request",message:"Please check your id"},{status:400});
         return Response.json({status:"updated Successlly"},{status:200});
@@ -48,7 +48,7 @@ export let PATCH = async(req,{params}) => {
 export let DELETE = async(_,{params}) => {
     try{
         await connectDB();
-        let product= await productModel.findByIdAndDelete(params.id);
+        let product= await ProductModel.findByIdAndDelete(params.id);
         if(!product)
             return Response.json({status:"Wrong request",message:"Please check your id"},{status:400});
         return Response.json({status:"Deleted Success"},{status:200});
